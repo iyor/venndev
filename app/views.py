398366a3b5c0f6084c92bb65c0591d8617b88253
Erik <erikejan@hotmail.com>
2016-09-24 19:50:35 +0200

    Initial functioning Hello world site

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import send_file
+from app import app
+
+@app.route("/")
+def index():
+    return send_file("templates/index.html")
+		
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
