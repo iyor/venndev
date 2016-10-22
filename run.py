4367176ae10d47c78ae05472808d443637c856dd
Erik <erikejan@hotmail.com>
2016-10-22 20:13:23 +0200

    Changed run configurations for nginx

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 
 from app import app
-app.run(debug=True) #Stage 1: localhost and clientside debug messages
+#app.run(debug=True) #Stage 1: localhost and clientside debug messages
 #app.run(debug=True, host='0.0.0.0') #Stage 2: clientside debug
-#app.run() #Stage 3: untested
+app.run(host='0.0.0.0', port=8080) #Stage 3: untested
+
